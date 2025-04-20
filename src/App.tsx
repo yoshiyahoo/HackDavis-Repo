769a346387993f560d57f1b036ee98093653b1fe
Zoheb Sharif <sharifzoheb@gmail.com>
2025-04-20 04:23:46 -0700

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Toaster } from "@/components/ui/toaster"
 import { Toaster as Sonner } from "@/components/ui/sonner"
 import { TooltipProvider } from "@/components/ui/tooltip"
@@ -5,23 +7,33 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Index from "./pages/Index"
 import NotFound from "./pages/NotFound"
+import Quiz from "./Quiz"
+import { useState } from "react"
+
 
 const queryClient = new QueryClient()
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-)
+const App = () => {
+  // State to control the quiz dialog visibility
+  const [showQuiz, setShowQuiz] = useState(false)
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            {/* Add a dedicated route for the quiz if you want to access it directly */}
+            <Route path="/quiz" element={<Quiz />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  )
+}
 
 export default App
